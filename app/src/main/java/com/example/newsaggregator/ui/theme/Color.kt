0ee02626c92cce9db32f5938134887b704a2df2c
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-01-26 12:46:38 +0200

    Add theme and colors

--- app/src/main/java/com/example/newsaggregator/ui/theme/Color.kt
+++ app/src/main/java/com/example/newsaggregator/ui/theme/Color.kt
@@ -12,3 +12,11 @@ val Pink40 = Color(0xFF7D5260)
 
 val Black = Color(0xFF000000)
 val RollingStone = Color(0xFF77797E)
+
+val White = Color(0xFFFFFFFF)
+val Magnolia = Color(0xFFF8F2FF)
+val LightWisteria = Color(0xFFC1ADE1)
+val Wisteria = Color(0xFF8F70C0)
+val Tolopea = Color(0xFF1B004D)
+val BurntSienna = Color(0xFFEB5757)
+val Grandis = Color(0xFFFFD485)
--- app/src/main/java/com/example/newsaggregator/ui/theme/Theme.kt
+++ app/src/main/java/com/example/newsaggregator/ui/theme/Theme.kt
@@ -16,25 +16,33 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Wisteria,
+    primaryContainer = LightWisteria,
+    secondary = Tolopea,
+    secondaryContainer = Grandis,
+    background = Magnolia,
+    surface = White,
+    error = BurntSienna,
+    onPrimary = White,
+    onSecondary = Black,
+    onBackground = Black,
+    onSurface = White,
+    onError = White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Wisteria,
+    primaryContainer = LightWisteria,
+    secondary = Tolopea,
+    secondaryContainer = Grandis,
+    background = Magnolia,
+    surface = White,
+    error = BurntSienna,
+    onPrimary = White,
+    onSecondary = Black,
+    onBackground = Black,
+    onSurface = Black,
+    onError = White
 )
 
 @Composable
