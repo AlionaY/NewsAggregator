63c54b7cd5cdd7d1ec694dfced09d94c5a249fcc
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-01-30 17:48:03 +0200

    Make sample of ProfileScreen

--- app/src/main/java/com/example/newsaggregator/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/newsaggregator/ui/screen/profile/ProfileScreen.kt
@@ -1,9 +1,33 @@
 package com.example.newsaggregator.ui.screen.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
-fun ProfileScreen(navController: NavController) {
+fun ProfileScreen(
+    navController: NavController,
+    viewModel: ProfileViewModel = hiltViewModel()
+) {
 
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth(),
+            onClick = { viewModel.login() }
+        ) {
+            Text(text = "Login")
+        }
+    }
 }
