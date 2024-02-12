90261af1744798a6801c675890d4086593a2eebb
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-12 17:46:21 +0200

    Delete SplashScreen

--- app/src/main/java/com/example/newsaggregator/ui/navigation/Navigation.kt
+++ app/src/main/java/com/example/newsaggregator/ui/navigation/Navigation.kt
@@ -7,15 +7,10 @@ import androidx.navigation.compose.composable
 import com.example.newsaggregator.ui.screen.home.HomeScreen
 import com.example.newsaggregator.ui.screen.profile.ProfileScreen
 import com.example.newsaggregator.ui.screen.search.SearchScreen
-import com.example.newsaggregator.ui.screen.splash.SplashScreen
 
 @Composable
 fun Navigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Routes.Home.route) {
-//        composable(Routes.Splash.route) {
-//            SplashScreen(navController)
-//        }
-
         composable(Routes.Home.route) {
             HomeScreen(navController)
         }
