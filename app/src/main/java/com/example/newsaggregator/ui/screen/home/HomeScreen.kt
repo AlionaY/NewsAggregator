31562dbad5fa80e78ce7c7a1df669d1a3bda860f
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-13 17:30:53 +0200

    Add loading circle to image; refactoring

--- app/src/main/java/com/example/newsaggregator/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/newsaggregator/ui/screen/home/HomeScreen.kt
@@ -1,42 +1,21 @@
 package com.example.newsaggregator.ui.screen.home
 
-import android.util.Log
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.compose.itemKey
-import coil.compose.AsyncImage
 
-//todo: add loading circle to screen and to end of the screen when loading new items
 //todo: add arrow to jump to top after 1st/2nd item isn`t visible
 //todo: navigate to screen with anime details
 //todo: show more info in item card
@@ -51,8 +30,6 @@ fun HomeScreen(
     val (animeItemsList) = viewState
 
     val animeList = animeItemsList.collectAsLazyPagingItems()
-    val lazyListState = rememberLazyListState()
-
 
     Box(
         modifier = Modifier
@@ -60,94 +37,19 @@ fun HomeScreen(
             .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-
-        if (animeList.loadState.refresh == LoadState.Loading) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(30.dp),
-                strokeWidth = 4.dp
-            )
-        } else {
-            LazyColumn(
-                state = lazyListState,
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-            ) {
-
-                items(
-                    count = animeList.itemCount,
-                    key = animeList.itemKey { it.id }
-                ) { index ->
-                    val item = animeList[index]
-
-                    AnimeItem(
-                        iconUrl = item?.images?.jpg?.imageUrl.orEmpty(),
-                        title = item?.title.orEmpty(),
-                        modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 4.dp)
-                            .fillMaxWidth()
-                    ) {
-                        item?.let { viewModel.onAnimeItemClick(it) }
-                    }
-                }
-
-                item {
-                    if (animeList.loadState.append == LoadState.Loading) {
-                        Row(
-                            modifier = Modifier
-                                .padding(vertical = 4.dp)
-                                .fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            CircularProgressIndicator(
-                                modifier = Modifier.size(16.dp),
-                                strokeWidth = 4.dp
-                            )
-                        }
-                    }
-                }
-
-                item { Spacer(modifier = Modifier.height(10.dp)) }
-            }
-        }
-    }
-}
-
-@Composable
-private fun AnimeItem(
-    iconUrl: String,
-    title: String,
-    modifier: Modifier = Modifier,
-    onItemClick: () -> Unit
-) {
-    val shape = RoundedCornerShape(10)
-    Card(
-        modifier = modifier
-            .clip(shape)
-            .clickable { onItemClick() },
-        shape = shape,
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 2.dp
-        )
-    ) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(6.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                AsyncImage(
-                    model = iconUrl,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .width(70.dp)
-                        .clip(RoundedCornerShape(10)),
+        when {
+            animeList.loadState.refresh == LoadState.Loading -> {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(30.dp),
+                    strokeWidth = 4.dp
                 )
-                Text(
-                    text = title,
-                    modifier = Modifier.padding(start = 10.dp),
-                    style = MaterialTheme.typography.bodyMedium
+            }
+
+            else -> {
+                AnimeList(
+                    animeList = animeList,
+                    modifier = Modifier.fillMaxSize(),
+                    onAnimeItemClick = { viewModel.onAnimeItemClick(it) }
                 )
             }
         }
