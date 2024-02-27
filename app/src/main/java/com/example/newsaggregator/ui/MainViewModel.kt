9ddcb15b6f7e80f94363e8398948578ea0048571
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2024-02-27 13:15:15 +0200

    Navigate through VM events value

--- app/src/main/java/com/example/newsaggregator/ui/MainViewModel.kt
+++ app/src/main/java/com/example/newsaggregator/ui/MainViewModel.kt
@@ -1,12 +1,25 @@
 package com.example.newsaggregator.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.events
+import androidx.lifecycle.viewModelScope
+import com.example.newsaggregator.ui.model.AppEvent
+import com.example.newsaggregator.ui.model.BottomNavBarItems
+import com.example.newsaggregator.ui.navigation.Routes
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-): ViewModel() {
-
-
+class MainViewModel @Inject constructor() : ViewModel() {
+    fun navigate(bottomBarItem: BottomNavBarItems) {
+        viewModelScope.launch {
+            events.emit(
+                AppEvent.Navigate(bottomBarItem.route) {
+                    launchSingleTop = true
+                    popUpTo(Routes.Home.route)
+                }
+            )
+        }
+    }
 }
